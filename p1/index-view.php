7aa9b1ff9a81f017eae72c66d69c9f889286cbe9
Susan Buck <susanbuck@fas.harvard.edu>
2021-09-28 20:14:46 +0000

    Include game mechanics

--- p1/index-view.php
+++ p1/index-view.php
@@ -2,17 +2,20 @@
 <html lang='en'>
 
 <head>
-    <title>Project 1</title>
+    <title>Project 1 - Coin Flip</title>
     <meta charset='utf-8'>
     <link href=data:, rel=icon>
 </head>
 
 <body>
-    <h1>Project 1</h1>
+    <h1>Project 1 - Coin Flip</h1>
 
     <h2>Mechanics</h2>
     <ul>
-        <li>...</li>
+        <li>Player A randomly picks a coin side - heads or tails.</li>
+        <li>A coin is randomly “flipped”, “landing” on either heads or tails.</li>
+        <li>If the coin landed on the same side Player A chose, they win.</li>
+        <li>Otherwise, Player B wins.</li>
     </ul>
 
     <h2>Results</h2>
